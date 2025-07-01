7758144790e3a31566cfdb1b06b7979e725e078c
Ahmet Yavas <ahmetyavas1940@gmail.com>
2025-07-01 14:24:18 +0300

    delete supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = 'https://qqmwpqmfrxpsrhsievlp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxbXdwcW1mcnhwc3Joc2lldmxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5ODQyMzQsImV4cCI6MjA1MjU2MDIzNH0.ecFn2cjbzr-g3reDmnckv53zkkXIJr61LCMkp2ynDIc';
+const supabaseUrl = '';
+const supabaseAnonKey = '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
